bb7676cd7a33da9fa7cc49d85a57511ee38be184
woutergins <wouter.gins@gmail.com>
2015-08-31 16:16:55 +0200

    Added documentation

--- fitting.py
+++ fitting.py
@@ -15,7 +15,36 @@ __all__ = ['chisquare_spectroscopic_fit', 'chisquare_fit', 'likelihood_fit']
 # CHI SQUARE FITTING ROUTINES #
 ###############################
 
-def model(params, spectrum, x, y, yerr, pearson, **kwargs):
+def model(params, spectrum, x, y, yerr, pearson=False):
+    """Model function for chisquare fitting routines as established
+    in this module.
+
+    Parameters
+    ----------
+    params: lmfit.Parameters
+        Instance of lmfit.Parameters object, to be assigned to the spectrum object.
+    spectrum: satlas.Spectrum
+        Instance of a satlas.Spectrum object, to be fitted to the data.
+    x: array_like
+        Experimental data for the x-axis.
+    y: array_like
+        Experimental data for the y-axis.
+    yerr: array_like
+        Experimental errorbars on the y-axis.
+    pearson: boolean
+        If True, use the square root of the fitted value as the
+        experimental uncertainty.
+
+    Returns
+    -------
+    NumPy array
+        Array containing the residuals for the given parameters, divided by the
+        experimental uncertainty
+
+    Note
+    ----
+    If a custom function is to be used for the calculation of the residual,
+    this function should be overwritten."""
     spectrum.params = params
     model = spectrum(x)
     if pearson:
@@ -23,8 +52,18 @@ def model(params, spectrum, x, y, yerr, pearson, **kwargs):
     return (y - model) / yerr
 
 def chisquare_spectroscopic_fit(spectrum, x, y, **kwargs):
-    """Use the :meth:`FitToData` method, automatically estimating the errors
-    on the counts by the square root."""
+    """Use the :func:`chisquare_fit` function, automatically estimating the errors
+    on the counts by the square root.
+
+    Parameters
+    ----------
+    spectrum: satlas.Spectrum object
+        Spectrum object containing all the information about the fit;
+        will be fitted to the given data.
+    x: array_like
+        Experimental data for the x-axis.
+    y: array_like
+        Experimental data for the y-axis."""
     x, y, _ = spectrum.sanitize_input(x, y)
     yerr = np.sqrt(y)
     yerr[np.isclose(yerr, 0.0)] = 1.0
@@ -33,22 +72,27 @@ def chisquare_spectroscopic_fit(spectrum, x, y, **kwargs):
 def chisquare_fit(spectrum, x, y, yerr, pearson=True, monitor=True):
     """Use a non-linear least squares minimization (Levenberg-Marquardt)
     algorithm to minimize the chi-square of the fit to data :attr:`x` and
-    :attr:`y` with errorbars :attr:`yerr`. Reasonable bounds are used on
-    parameters, and the user-supplied :attr:`self._vary` dictionary is
-    consulted to see if a parameter should be varied or not.
+    :attr:`y` with errorbars :attr:`yerr`.
 
     Parameters
     ----------
+    spectrum: satlas.Spectrum object
+        Spectrum object containing all the information about the fit;
+        will be fitted to the given data.
     x: array_like
-        Frequency of the data, in MHz.
+        Experimental data for the x-axis.
     y: array_like
-        Counts corresponding to :attr:`x`.
+        Experimental data for the y-axis.
     yerr: array_like
         Error bars on :attr:`y`.
     pearson: boolean, optional
         Selects if the normal or Pearson chi-square statistic is used.
         The Pearson chi-square uses the model value to estimate the
-        uncertainty. Defaults to :attr:`True`."""
+        uncertainty. Defaults to :attr:`True`.
+    monitor: boolean, optional
+        If True, a plot will be displayed during the fitting which gives the
+        reduced chisquare statistic in function of the iteration
+        number."""
 
     x, y, yerr = spectrum.sanitize_input(x, y, yerr)
 
@@ -111,20 +155,94 @@ class PriorParameter(lm.Parameter):
 theta_array = np.linspace(-3, 3, 1024)
 
 def x_err_calculation(spectrum, x, y, s, func):
+    """Calculates the loglikelihood for a spectrum given
+    x and y values. Incorporates a common given error on
+    the x-axis.
+
+    Parameters
+    ----------
+    spectrum: satlas.Spectrum object
+        Spectrum object set to current parameters.
+    x: array_like
+        Experimental data for the x-axis.
+    y: array_like
+        Experimental data for the y-axis.
+    s: float
+        Experimental uncertainty on the x-axis
+    func: function
+        Function taking (y_data, y_model) as input,
+        and returning the loglikelihood that the data is
+        drawn from a distribution characterized by the model.
+
+    Returns
+    -------
+    array_like
+
+    Note
+    ----
+    This method uses the FFT algorithm to quickly calculate
+    a convolution integral. If greater accuracy is required,
+    change :attr:`satlas.fitting.theta_array` to a suitable
+    range and length."""
     x, theta = np.meshgrid(x, theta_array)
     y, _ = np.meshgrid(y, theta_array)
-    p = func(y, spectrum(x + theta))
+    p = np.exp(func(y, spectrum(x + theta)))
     g = np.exp(-(theta / s)**2 / 2) / s
     return np.log(np.fft.irfft(np.fft.rfft(p) * np.fft.rfft(g))[:, -1])
 
 def lnprob(params, spectrum, x, y, func):
+    """Calculates the logarithm of the probability that the data fits
+    the model given the current parameters.
+
+    Parameters
+    ----------
+    params: lmfit.Parameters object with satlas.PriorParameters
+        Group of parameters for which the fit has to be evaluated.
+    spectrum: satlas.Spectrum object
+        Spectrum object containing all the information about the fit;
+        will be fitted to the given data.
+    x: array_like
+        Experimental data for the x-axis.
+    y: array_like
+        Experimental data for the y-axis.
+    func: function
+        Function calculating the loglikelihood of y_data being drawn from
+        a distribution characterized by y_model.
+
+    Note
+    ----
+    The prior is first evaluated for the parameters. If this is
+    not finite, the values are rejected from consideration by
+    immediately returning -np.inf."""
     lp = lnprior(params)
     if not np.isfinite(lp):
         return -np.inf
-    res = lp + np.sum(loglikelihood(spectrum, params, x, y, func))
+    res = lp + np.sum(loglikelihood(params, spectrum, x, y, func))
     return res
 
 def lnprior(params):
+    """Calculates the logarithm of the prior given the parameter
+    values. This is independent of the data to be fitted to.
+
+    Parameters
+    ----------
+    params: lmfit.Parameters with satlas.PriorParameters
+        Collection of satlas.PriorParameters, which
+        contain also prior bounds, so the boundary
+        calculations are not triggered before this point.
+        Allows easy interfacing with the emcee package.
+
+    Returns
+    -------
+    1.0 or -np.inf
+        Calculates a flat prior: returns 1.0 if all
+        parameters are inside their boundaries,
+        reutnr -np.inf if one of them is not.
+
+    Note
+    ----
+    In case a custom prior distribution is required,
+    this function has to be overwritten."""
     for key in params.keys():
         try:
             leftbound, rightbound = (params[key].priormin,
@@ -137,11 +255,39 @@ def lnprior(params):
             return -np.inf
     return 1.0
 
-def loglikelihood(spectrum, params, x, y, func):
+def loglikelihood(params, spectrum, x, y, func):
+    """Given a parameters object, a Spectrum object, experimental data
+    and a loglikelihood function, calculates the loglikelihood for
+    all data points.
+
+    Parameters
+    ----------
+    params: lmfit.Parameters object with satlas.PriorParameters
+        Group of parameters for which the fit has to be evaluated.
+    spectrum: satlas.Spectrum object
+        Spectrum object containing all the information about the fit;
+        will be fitted to the given data.
+    x: array_like
+        Experimental data for the x-axis.
+    y: array_like
+        Experimental data for the y-axis.
+    func: function
+        Function calculating the loglikelihood of y_data being drawn from
+        a distribution characterized by y_model.
+
+    Returns
+    -------
+    array_like
+        Array containing the loglikelihood for each seperate datapoint."""
     spectrum.params = params
+    # If any value of the evaluated spectrum is below 0,
+    # or the difference between the minimum and maximum is too low,
+    # reject the parameter values.
     if any([np.isclose(X.min(), X.max(), atol=0.1)
             for X in spectrum.seperate_response(x)]) or any(spectrum(x) < 0):
         return -np.inf
+    # If a value is given to the uncertainty on the x-values, use the adapted
+    # function.
     if params['sigma_x'].value > 0:
         s = params['sigma_x'].value
         return_value = x_err_calculation(spectrum, x, y, s, func)
@@ -149,7 +295,38 @@ def loglikelihood(spectrum, params, x, y, func):
         return_value = func(y, spectrum(x))
     return return_value
 
-def likelihood_fit(spectrum, x, y, xerr=0, vary_sigma=False, func=llh.poisson_llh, walking=True, **kwargs):
+def likelihood_fit(spectrum, x, y, xerr=0, vary_sigma=False, func=llh.poisson_llh, walking=True, walk_kws={}):
+    """Fits the given spectrum to the given data using the Maximum Likelihood Estimation technique.
+
+    Parameters
+    ----------
+    spectrum: satlas.Spectrum
+        Spectrum to be fitted to the data.
+    x: array_like
+        Experimental data for the x-axis.
+    y: array_like
+        Experimental data for the y-axis.
+
+    Other parameters
+    ----------------
+    xerr: float, optional
+        Estimated value for the uncertainty on the x-values.
+        Set to 0 to ignore this uncertainty. Defaults to 0.
+    vary_sigma: boolean, optional
+        If True, the uncertainty on the x-values will be
+        treated as a parameter and will be optimized.
+    func: function, optional
+        Used to calculate the loglikelihood that the data is drawn
+        from a distribution given a model value. Should accept
+        input as (y_data, y_model). Defaults to the Poisson
+        loglikelihood.
+    walking: boolean, optional
+        If True, the uncertainty on the parameters is estimated
+        by performing a random walk in parameter space and
+        evaluating the loglikelihood.
+    walk_kws: dictionary
+        Contains the keywords for the :func:`likelihood_walk`
+        function, used if walking is set to True."""
     def negativeloglikelihood(*args, **kwargs):
         return -lnprob(*args, **kwargs)
 
@@ -163,11 +340,51 @@ def likelihood_fit(spectrum, x, y, xerr=0, vary_sigma=False, func=llh.poisson_ll
     spectrum.mle_result = result.message
 
     if walking:
-        likelihood_walk(spectrum, x, y, func=func, **kwargs)
+        likelihood_walk(spectrum, x, y, func=func, **walk_kws)
     return None
 
 def likelihood_walk(spectrum, x, y, func=llh.poisson_llh, nsteps=2000, walkers=20, burnin=10.0,
                     verbose=True, store_walks=False):
+    """Calculates the uncertainty on MLE-optimized parameter values
+    by performing a random walk through parameter space and comparing
+    the resulting loglikelihood values. For more information,
+    see the emcee package.
+
+    Parameters
+    ----------
+    spectrum: satlas.Spectrum
+        Spectrum to be fitted to the data.
+    x: array_like
+        Experimental data for the x-axis.
+    y: array_like
+        Experimental data for the y-axis.
+
+    Other parameters
+    ----------------
+    func: function, optional
+        Used to calculate the loglikelihood that the data is drawn
+        from a distribution given a model value. Should accept
+        input as (y_data, y_model). Defaults to the Poisson
+        loglikelihood.
+    walkers: integer, optional
+        Sets the number of walkers to be used for the random walk.
+        The number of walkers should never be less than twice the
+        number of parameters. For more information on this, see
+        the emcee documentation. Defaults to 20 walkers.
+    nsteps: integer, optional
+        Determines how many steps each walker should take.
+        Defaults to 2000 steps.
+    burnin: float < 100.0
+        Sets the percentage of the walk to be considered burn-in
+        (steps for which the walk has not started exploring the space yet).
+        Defaults to 10.0 percent.
+    verbose: boolean, optional
+        If True, a progressbar is printed and updated every second.
+        This progressbar displays the progress of the walk, with a primitive
+        estimate of the remaining time in the calculation.
+    stor_walks: boolean, optional
+        If True, the data from the walks is stored in the spectrum
+        object under the attribute :attr:`walks`. Defaults to False."""
 
     params = spectrum.mle_fit
     var_names = []
@@ -240,5 +457,3 @@ def likelihood_walk(spectrum, x, y, func=llh.poisson_llh, nsteps=2000, walkers=2
     if store_walks:
         spectrum.walks = [(name, sampler.chain[:, :, i].T)
                       for i, name in enumerate(var_names)]
-    else:
-        spectrum.walks = None
