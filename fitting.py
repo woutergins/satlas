Fixed overflow bug

fitting.py
@@ -174,7 +174,7 @@ theta_array = np.linspace(-5, 5, 2**10)
 _x_err_calculation_stored = {}
 sqrt2pi = np.sqrt(2*np.pi)
 
-def likelihood_x_err(spectrum, x, y, s, func):
+def likelihood_x_err(spectrum, x, y, xerr, func):
     """Calculates the loglikelihood for a spectrum given
     x and y values. Incorporates a common given error on
     the x-axis.
@@ -187,8 +187,8 @@ def likelihood_x_err(spectrum, x, y, s, func):
         Experimental data for the x-axis.
     y: array_like
         Experimental data for the y-axis.
-    s: float
-        Experimental uncertainty on the x-axis
+    xerr: float
+        Experimental uncertainty on *x*.
     func: function
         Function taking (*y_data*, *y_model*) as input,
         and returning the loglikelihood that the data is
@@ -204,20 +204,37 @@ def likelihood_x_err(spectrum, x, y, s, func):
     a convolution integral. If greater accuracy is required,
     change *satlas.fitting.theta_array* to a suitable
     range and length."""
+    # Cache already calculated values:
+    # - x_grid
+    # - y_grid
+    # - FFT of x-uncertainty
+    # Note that this works only if the uncertainty remains the same.
+    # If a parameter approach is desired, this needs to be changed.
     key = hash(x.data.tobytes()) + hash(y.data.tobytes())
     if key in _x_err_calculation_stored:
-        x_grid, y_grid, theta, g_top = _x_err_calculation_stored[key]
+        x_grid, y_grid, theta, rfft_g = _x_err_calculation_stored[key]
     else:
         x_grid, theta = np.meshgrid(x, theta_array)
         y_grid, _ = np.meshgrid(y, theta_array)
         g_top = (np.exp(-theta*theta * 0.5)).T
-        _x_err_calculation_stored[key] = x_grid, y_grid, theta, g_top
-    p = (np.exp(func(y_grid, spectrum(x_grid + s * theta)))).T
-    g = g_top / (sqrt2pi * s)
-    integral_value = np.fft.irfft(np.fft.rfft(p) * np.fft.rfft(g))[:, -1]
-    return np.log(integral_value)
+        g = (g_top.T / (sqrt2pi * xerr)).T
+        rfft_g = np.fft.rfft(g)
+        _x_err_calculation_stored[key] = x_grid, y_grid, theta, rfft_g
+    # Calculate the loglikelihoods for the grid of uncertainty.
+    # Each column is a new datapoint.
+    vals = func(y_grid, spectrum(x_grid + xerr * theta))
+    # To avoid overflows, subtract the maximal values from each column.
+    mod = vals.max(axis=0)
+    vals_mod = vals - mod
+    p = (np.exp(vals_mod)).T
+    # Perform the convolution.
+    integral_value = np.fft.irfft(np.fft.rfft(p) * rfft_g)[:, -1]
+    # After taking the logarithm, add the maximal values again.
+    # The subtraction becomes multiplication (with an exponential) after the exponential,
+    # shifts through the integral, and becomes an addition (due to the logarithm).
+    return np.log(integral_value) + mod
 
-def likelihood_lnprob(params, spectrum, x, y, func):
+def likelihood_lnprob(params, spectrum, x, y, xerr, func):
     """Calculates the logarithm of the probability that the data fits
     the model given the current parameters.
 
@@ -232,6 +249,8 @@ def likelihood_lnprob(params, spectrum, x, y, func):
         Experimental data for the x-axis.
     y: array_like
         Experimental data for the y-axis.
+    xerr: array_like
+        Uncertainty values on *x*.
     func: function
         Function calculating the loglikelihood of y_data being drawn from
         a distribution characterized by y_model.
@@ -244,7 +263,7 @@ def likelihood_lnprob(params, spectrum, x, y, func):
     lp = likelihood_lnprior(params)
     if not np.isfinite(lp):
         return -np.inf
-    res = lp + np.sum(likelihood_loglikelihood(params, spectrum, x, y, func))
+    res = lp + np.sum(likelihood_loglikelihood(params, spectrum, x, y, xerr, func))
     return res
 
 def likelihood_lnprior(params):
@@ -282,7 +301,7 @@ def likelihood_lnprior(params):
             return -np.inf
     return 1.0
 
-def likelihood_loglikelihood(params, spectrum, x, y, func):
+def likelihood_loglikelihood(params, spectrum, x, y, xerr, func):
     """Given a parameters object, a Spectrum object, experimental data
     and a loglikelihood function, calculates the loglikelihood for
     all data points.
@@ -298,6 +317,8 @@ def likelihood_loglikelihood(params, spectrum, x, y, func):
         Experimental data for the x-axis.
     y: array_like
         Experimental data for the y-axis.
+    xerr: array_like
+        Experimental data on *x*.
     func: function
         Function calculating the loglikelihood of y_data being drawn from
         a distribution characterized by y_model.
@@ -315,14 +336,13 @@ def likelihood_loglikelihood(params, spectrum, x, y, func):
         return -np.inf
     # If a value is given to the uncertainty on the x-values, use the adapted
     # function.
-    if params['sigma_x'].value > 0:
-        s = params['sigma_x'].value
-        return_value = likelihood_x_err(spectrum, x, y, s, func)
-    else:
+    if xerr is None or np.allclose(0, xerr):
         return_value = func(y, spectrum(x))
+    else:
+        return_value = likelihood_x_err(spectrum, x, y, xerr, func)
     return return_value
 
-def likelihood_fit(spectrum, x, y, xerr=0, vary_sigma=False, func=llh.poisson_llh, method='L-BFGS-B', method_kws={}, walking=False, walk_kws={}):
+def likelihood_fit(spectrum, x, y, xerr=None, func=llh.poisson_llh, method='L-BFGS-B', method_kws={}, walking=False, walk_kws={}):
     """Fits the given spectrum to the given data using the Maximum Likelihood Estimation technique.
     The given function is used to calculate the loglikelihood. After the fit, the message
     from the optimizer is printed and returned.
@@ -338,13 +358,9 @@ def likelihood_fit(spectrum, x, y, xerr=0, vary_sigma=False, func=llh.poisson_ll
 
     Other parameters
     ----------------
-    xerr: float, optional
+    xerr: array_like, optional
         Estimated value for the uncertainty on the x-values.
-        Set to 0 to ignore this uncertainty. Defaults to 0.
-    vary_sigma: boolean, optional
-        If True, the uncertainty on the x-values will be
-        treated as a parameter and will be optimized.
-        Defaults to False.
+        Set to *None* to ignore this uncertainty. Defaults to *None*.
     func: function, optional
         Used to calculate the loglikelihood that the data is drawn
         from a distribution given a model value. Should accept
@@ -376,8 +392,7 @@ def likelihood_fit(spectrum, x, y, xerr=0, vary_sigma=False, func=llh.poisson_ll
 
     x, y, _ = spectrum._sanitize_input(x, y)
     params = spectrum.params
-    params.add('sigma_x', value=xerr, vary=vary_sigma, min=0)
-    result = lm.Minimizer(negativeloglikelihood, params, fcn_args=(spectrum, x, y, func))
+    result = lm.Minimizer(negativeloglikelihood, params, fcn_args=(spectrum, x, y, xerr, func))
     success = result.scalar_minimize(method=method, **method_kws)
     spectrum.params = result.params
     spectrum.mle_fit = result.params
@@ -385,10 +400,10 @@ def likelihood_fit(spectrum, x, y, xerr=0, vary_sigma=False, func=llh.poisson_ll
     spectrum.mle_likelihood = negativeloglikelihood(params, spectrum, x, y, func)
 
     if walking:
-        likelihood_walk(spectrum, x, y, func=func, **walk_kws)
+        likelihood_walk(spectrum, x, y, xerr=xerr, func=func, **walk_kws)
     return success, result.message
 
-def likelihood_walk(spectrum, x, y, func=llh.poisson_llh, nsteps=2000, walkers=20, burnin=10.0,
+def likelihood_walk(spectrum, x, y, xerr=None, func=llh.poisson_llh, nsteps=2000, walkers=20, burnin=10.0,
                     verbose=True, store_walks=False):
     """Calculates the uncertainty on MLE-optimized parameter values
     by performing a random walk through parameter space and comparing

utilities.py
@@ -74,7 +74,7 @@ def weighted_average(x, sigma, axis=None):
     Xstat = 1 / Xstat
     return Xm, np.maximum.reduce([Xstat, Xscatt], axis=axis) ** 0.5
 
-def _make_axes_grid(no_variables, padding=2, cbar_size=0.5, axis_padding=0.5):
+def _make_axes_grid(no_variables, padding=2, cbar_size=0.5, axis_padding=0.5, cbar=True):
     """Makes a triangular grid of axes, with a colorbar axis next to it.
 
     Parameters
@@ -97,6 +97,8 @@ def _make_axes_grid(no_variables, padding=2, cbar_size=0.5, axis_padding=0.5):
     padding, cbar_size, axis_padding = (padding * 0.393700787,
                                         cbar_size * 0.393700787,
                                         axis_padding * 0.393700787)
+    if not cbar:
+        cbar_size = 0
 
     # Generate the figure, convert padding to percentages.
     fig = plt.figure()
@@ -123,7 +125,7 @@ def _make_axes_grid(no_variables, padding=2, cbar_size=0.5, axis_padding=0.5):
     for i, I in zip(range(no_variables), reversed(range(no_variables))):
         for j in reversed(range(no_variables)):
             # Only create axes on the lower triangle.
-            if I+j < no_variables:
+            if I + j < no_variables:
                 # Share the x-axis with the plot on the diagonal,
                 # directly above the plot.
                 sharex = axes[j, j] if i != j else None
@@ -159,9 +161,12 @@ def _make_axes_grid(no_variables, padding=2, cbar_size=0.5, axis_padding=0.5):
 
     height = 1 - 2 * up_padding
 
-    cbar = plt.axes([left_edge, bottom_edge, width, height])
-    plt.setp(cbar.get_xticklabels(), visible=False)
-    plt.setp(cbar.get_yticklabels(), visible=False)
+    if cbar:
+        cbar = plt.axes([left_edge, bottom_edge, width, height])
+        plt.setp(cbar.get_xticklabels(), visible=False)
+        plt.setp(cbar.get_yticklabels(), visible=False)
+    else:
+        cbar = None
     return fig, axes, cbar
 
 def generate_correlation_map(spectrum, x_data, y_data, method='chisquare', filter=None, resolution_diag=20, resolution_map=15, fit_kws={}):
@@ -197,11 +202,7 @@ def generate_correlation_map(spectrum, x_data, y_data, method='chisquare', filte
     resolution_map: int
         Number of points along each dimension for the meshgrids.
     fit_kws: dictionary
-        Dictionary of keywords to pass on to the fitting routine.
-
-    Warning
-    -------
-    At the moment, only the chisquare fitting routine is selected."""
+        Dictionary of keywords to pass on to the fitting routine."""
     from . import fitting
 
     def fit_new_value(value, spectrum, params, params_name, x, y, orig_value, func):
@@ -223,7 +224,7 @@ def generate_correlation_map(spectrum, x_data, y_data, method='chisquare', filte
                 print('Fitting did not converge, carrying on...')
         return getattr(spectrum, attr) - orig_value
 
-    #  Save the original chisquare and parameters for later use
+    # Save the original goodness-of-fit and parameters for later use
     mapping = {'chisquare': (fitting.chisquare_spectroscopic_fit, 'chisqr'),
                'mle': (fitting.likelihood_fit, 'mle_likelihood')}
     func, attr = mapping.pop(method.lower(), (fitting.chisquare_spectroscopic_fit, 'chisqr'))
@@ -239,7 +240,7 @@ def generate_correlation_map(spectrum, x_data, y_data, method='chisquare', filte
             no_params += 1
             param_names.append(p)
 
-    fig, axes, cbar = _make_axes_grid(no_params, axis_padding=2.0)
+    fig, axes, cbar = _make_axes_grid(no_params, axis_padding=2.0, cbar=no_params > 1)
 
     # Make the plots on the diagonal: plot the chisquare/likelihood
     # for the best fitting values while setting one parameter to
@@ -262,11 +263,12 @@ def generate_correlation_map(spectrum, x_data, y_data, method='chisquare', filte
             ax.set_ylabel(r'$\Delta\mathcal{L}$')
 
         # Extract the uncertainty on this parameter,
-        # set to 50 is this is not set yet. Also
+        # set to 10% is this is not set yet. Also
         # extract the value.
-        stderr = spectrum.params[param_names[i]].stderr
-        stderr = stderr if stderr is not None else 50
         value = spectrum.params[param_names[i]].value
+        stderr = spectrum.params[param_names[i]].stderr
+        draw = stderr is not None
+        stderr = stderr if stderr is not None else 0.1 * value
 
         # Keep the parameter fixed, and let it vary (with given number of points)
         # in a deviation of 3 sigma in both directions.
@@ -287,8 +289,9 @@ def generate_correlation_map(spectrum, x_data, y_data, method='chisquare', filte
         # For the loglikelihood, this need to be reduced by a factor of 2.
         ax.axhline(1-0.5*(method.lower()=='mle'), ls="dashed")
         # Indicate the used interval.
-        ax.axvline(value + stderr)
-        ax.axvline(value - stderr)
+        if draw:
+            ax.axvline(value + stderr)
+            ax.axvline(value - stderr)
         # Restore the parameters.
         spectrum.params = orig_params
         fitting.chisquare_spectroscopic_fit(spectrum, x_data, y_data, **fit_kws)
@@ -306,13 +309,13 @@ def generate_correlation_map(spectrum, x_data, y_data, method='chisquare', filte
         if i == no_params - 1:
             ax.set_xlabel(x_name)
         x_value, x_stderr = params[x_name].value, params[x_name].stderr
-        x_stderr = x_stderr if x_stderr is not None else 50
+        x_stderr = x_stderr if x_stderr is not None else 0.1 * x_value
         params[x_name].vary = False
-        x_range = np.linspace(x_value - 5*x_stderr, x_value + 5*x_stderr, resolution_map)
+        x_range = np.linspace(x_value - 5 * x_stderr, x_value + 5 * x_stderr, resolution_map)
         y_value, y_stderr = params[y_name].value, params[y_name].stderr
-        y_stderr = y_stderr if y_stderr is not None else 50
+        y_stderr = y_stderr if y_stderr is not None else 0.1 * y_value
         params[y_name].vary = False
-        y_range = np.linspace(y_value - 5*y_stderr, y_value + 5*y_stderr, resolution_map)
+        y_range = np.linspace(y_value - 5 * y_stderr, y_value + 5 * y_stderr, resolution_map)
         X, Y = np.meshgrid(x_range, y_range)
         Z = np.zeros(X.shape)
         i_indices, j_indices = np.indices(Z.shape)