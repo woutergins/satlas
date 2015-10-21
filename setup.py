b60f0503f50d71457431e5d94e06025b98e47ddb
woutergins <wouter.gins@gmail.com>
2015-10-21 18:07:53 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name='satlas',
   packages=['satlas'], # this must be the same as the name above
-  version='0.1.0b1',
+  version='0.1.0b4',
   description='This Python package has been created with the goal of creating an easier interface for the analysis of data gathered from laser spectroscopy experiments. Support for fitting the spectra, using both chi2-fitting and Maximum Likelihood Estimation routines, are present.',
   author='Wouter Gins',
   author_email='woutergins@gmail.com',
   url='https://woutergins.github.io/satlas/',
-  download_url='https://github.com/woutergins/satlas/tarball/0.1', # I'll explain this in a second
+  download_url='https://github.com/woutergins/satlas/tarball/0.1.0b4', # I'll explain this in a second
   keywords=['physics', 'hyperfine structure', 'fitting'], # arbitrary keywords
   install_requires=['numpy',
                     'scipy',
