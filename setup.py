6ccde0e20d406fbf2c27fa99e167448c9e44d855
Wouter Gins <wouter.gins@gmail.com>
2017-06-22 09:51:34 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 exec(open('satlas/version.py').read())
 setup(
   name='satlas',
-  packages=['satlas', 'satlas.stats', 'satlas.stats.emcee', 'satlas.tqdm', 'satlas.styles', 'satlas.utilities', 'satlas.models', 'satlas.example'],
+  packages=['satlas', 'satlas.stats', 'satlas.stats.emcee', 'satlas.tqdm', 'satlas.utilities', 'satlas.models', 'satlas.example'],
   package_data={'': ['*.txt']},
   version=__release__,
   description='This Python package has been created with the goal of creating an easier interface for the analysis of data gathered from laser spectroscopy experiments. Support for fitting the spectra, using both chi2-fitting and Maximum Likelihood Estimation routines, are present.',
@@ -12,7 +12,6 @@ setup(
   license='MIT',
   download_url='https://github.com/woutergins/satlas/archive/master.zip',
   keywords=['physics', 'hyperfine structure', 'fitting'],
-  },
   install_requires=['numpy>=1.5',
                     'scipy>=0.13',
                     'lmfit',
